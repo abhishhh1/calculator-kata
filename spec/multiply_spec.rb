ee89e7a82bb00e4c12b7c3c4dbb2e9c0937f657a
abhishhh1 <abhishhh1@gmail.com>
2022-04-09 19:18:07 +0530

    add multiply specs

--- spec/multiply_spec.rb
+++ spec/multiply_spec.rb
@@ -6,6 +6,36 @@ require 'rails'
 
 RSpec.describe 'Calculator' do
   context 'Multiply Numbers' do
+
+    it 'When only 1 number' do
+      service = ::Calculator::Public::Multiply.new('5')
+      result = service.calculate
+      expect(result).to eq 5
+    end
+
+    it 'When 2 numbers(comma separated)' do
+      service = ::Calculator::Public::Multiply.new('2,3')
+      result = service.calculate
+      expect(result).to eq 6
+    end
+
+    it 'When more than 2 numbers(comma separated)' do
+      service = ::Calculator::Public::Multiply.new('1,2,3,4,5')
+      result = service.calculate
+      expect(result).to eq 120
+    end
+
+    it 'When 2 numbers(line separated)' do
+      service = ::Calculator::Public::Multiply.new('2\n3')
+      result = service.calculate
+      expect(result).to eq 6
+    end
+
+    it 'When line and comma separated' do
+      service = ::Calculator::Public::Multiply.new('4\n2,3')
+      result = service.calculate
+      expect(result).to eq 24
+    end
   end
 end
 
